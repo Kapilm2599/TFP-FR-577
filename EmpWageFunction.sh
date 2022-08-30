f9e16d16bb77ffdc2b6008a0ba312af2f97b0ba6
kapilmahajan123 <montyempire123gmail.com>
2022-08-30 19:04:28 +0530

    [kapil]-program to check Employee Wage Function

---
+++ EmpWageFunction.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -x
+
+function calculateWorkingHour() {
+        case $1 in
+                0)
+                        workingHour=0;
+                        ;;
+
+                1)
+                        workingHour=8;
+                        ;;
+                2)
+                        workingHour=4;
+                        ;;
+        esac;
+        echo $workingHour;
+}
+
+perHourSalary=20;
+totalSalary=0;
+totalWorkingHour=0;
+day=1;
+
+while [[ $day -le 20 && $totalWorkingHour -lt 40 ]]
+do
+        wHour=$(calculateWorkingHour $((RANDOM%3)));
+        totalWorkingHour=$(($totalWorkingHour + $wHour));
+        if [ $totalWorkingHour -gt 40 ]
+        then
+                totalWorkingHour=$(($totalWorkingHour - $wHour));
+                break;
+        fi
+        salary=$(($perHourSalary * $wHour));
+        totalSalary=$(($totalSalary + $salary));
+        ((day++));
+done
+echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour)";
